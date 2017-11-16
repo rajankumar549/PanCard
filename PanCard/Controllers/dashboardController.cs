4ca08c4c078d7117e60eacbaf76e31f4af65fd2c
Pallavi Mandal <33718718+PallaviMandal@users.noreply.github.com>
2017-11-16 22:57:29 +0530

    apply  form bana  diava

---
+++ PanCard/Controllers/dashboardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PanCard.Controllers
+{
+    public class dashboardController : Controller
+    {
+        // GET: dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Apply()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
